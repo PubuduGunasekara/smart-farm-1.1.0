f03f1b33a9e9cf532cbeed21e89e22789edddf0b
pubudu praneeth gunasekara <38062640+PubuduPraneeth@users.noreply.github.com>
2021-03-29 00:42:31 +0530

    latest

--- App.js
+++ App.js
@@ -4,7 +4,13 @@ import { View, LogBox } from "react-native";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  /**put your firebase connetction details */
+  apiKey: "AIzaSyAhEvHVYS7FdViAOXb6z3LEYAOgRcDguNQ",
+  authDomain: "smart-farm-project-7139a.firebaseapp.com",
+  projectId: "smart-farm-project-7139a",
+  storageBucket: "smart-farm-project-7139a.appspot.com",
+  messagingSenderId: "1085026915732",
+  appId: "1:1085026915732:web:3abcaf23951bf7cc096b81",
+  measurementId: "G-253JV0D0YB",
 };
 
 if (firebase.apps.length === 0) {
